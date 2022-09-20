b3ded936697033f13d382745b447067b0044a24c
vladislav <vl_evlashin@mail.ru>
2022-09-21 00:15:40 +0300

    Подправил баги, исправил ошибки

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { diffDates, diffToHtml } from "./datecalc.js";
 import { formatError } from "./utils.js";
 import { timer } from './timer.js'
 
+const timerCount = document.querySelector('.timer-count');
 const timerInput = document.querySelector('#timer')
 const buttonStart = document.querySelector('.button-start')
+const buttonStop = document.querySelector('.button-stop');
 
 const dateCalcForm = document.getElementById("datecalc");
 const dateCalcResult = document.getElementById("datecalc__result");
@@ -12,11 +14,19 @@ dateCalcForm.addEventListener("submit", handleCalcDates);
 buttonStart.addEventListener('click', setTimer)
 
 function setTimer(event) {
-    event.preventDefault();
-    timer(timerInput.value)
+    if (timerCount.textContent == '' ||
+        timerCount.textContent == 'Осталось: 0 секунд' ||
+        timerCount.textContent == `Осталось: ${timerInput.value} секунд`) {
+        event.preventDefault();
+        timer(timerInput.value)
+    } else {
+        timerCount.textContent = 'Таймер уже запущен!'
+        console.log('aasd')
+    }
+
 };
 function clearTime(event) {
-
+    timerCount.textContent = timerCount.textContent;
 }
 function handleCalcDates(event) {
     dateCalcResult.innerHTML = "";
--- src/timer.js
+++ src/timer.js
@@ -1,7 +1,13 @@
 
 export function timer(count) {
     const timerCount = document.querySelector('.timer-count');
+    const buttonStop = document.querySelector('.button-stop');
+    const timerInput = document.querySelector('#timer')
     const interval = setInterval(() => {
+        buttonStop.addEventListener('click', () => {
+            timerInput.value = count + 1;
+            clearInterval(interval)
+        })
         if (count <= 0) {
             const audio = new Audio('./sound/tada.mp3');
             audio.play();
